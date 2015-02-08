1ae2d001b474ba3d5f57c411e367c986eea931df
caneroj1 <caneroj1@tcnj.edu>
2015-02-08 16:27:04 -0500

    changed the js file.

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,32 +1,7 @@
 $(function() {
-  var startingIndex = $('#carousel-videos').data('start');
-  var videoIndex = startingIndex;
   $('#carousel-videos').on('slide.bs.carousel', function (event) {
-    if(event.direction === "right") {
-      videoIndex -= 1
-      if(videoIndex < startingIndex) {
-        videoIndex = parseInt($('#carousel-videos').data('total')) + startingIndex - 1;
-      }
-
-      $('#video-title').text($('#' + videoIndex).data('title'));
-      $('#video-date').text($('#' + videoIndex).data('date'));
-    }
-    else {
-      videoIndex = (videoIndex + 1) % (parseInt($('#carousel-videos').data('total')) + startingIndex);
-      if(videoIndex < startingIndex) {
-        videoIndex = startingIndex
-      }
-
-      $('#video-title').text($('#' + videoIndex).data('title'));
-      $('#video-date').text($('#' + videoIndex).data('date'));
-    }
+    $('#video-title').text($(event.relatedTarget).children('.video-container').data('title'));
+    $('#video-date').text($(event.relatedTarget).children('.video-container').data('date'));
   });
-
-  $('.picker').on('click', function() {
-    videoIndex = $(this).data("slide-id");
-    $('#video-title').text($('#' + videoIndex).data('title'));
-    $('#video-date').text($('#' + videoIndex).data('date'));
-  });
-
   if ($('#datepicker').length > 0) $("#datepicker").datepicker();
 });
